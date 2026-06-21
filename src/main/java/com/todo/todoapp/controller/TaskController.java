3279c544f4d00edc6d19bf216aca3b1205b0e2af
Rohit Gele <rohitgele@gmail.com>
2026-06-21 21:40:11 +0530

    tasks.html

--- src/main/java/com/todo/todoapp/controller/TaskController.java
+++ src/main/java/com/todo/todoapp/controller/TaskController.java
@@ -1,5 +1,31 @@
 package com.todo.todoapp.controller;
 
-public class TaskController {
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.todo.todoapp.models.Task;
+import com.todo.todoapp.services.TaskService;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+	
+	private final TaskService taskService;
+	
+	public TaskController(TaskService taskService) {
+		this.taskService = taskService;
+	}
+	
+	@GetMapping
+	public String getTasks(Model model) {
+		List<Task> tasks = taskService.getAllTasks();
+		model.addAttribute("tasks", tasks);
+		return "tasks";
+	}
 }
+
--- src/main/java/com/todo/todoapp/services/TaskService.java
+++ src/main/java/com/todo/todoapp/services/TaskService.java
@@ -1,8 +1,21 @@
 package com.todo.todoapp.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.todo.todoapp.models.Task;
+import com.todo.todoapp.repository.TaskRepository;
 
 @Service
 public class TaskService {
 
+	private final TaskRepository taskRepository;
+	
+	public TaskService(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+	public List<Task> getAllTasks() {
+		return taskRepository.findAll();
+	}
 }
